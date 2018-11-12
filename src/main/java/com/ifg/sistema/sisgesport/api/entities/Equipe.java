d9f6c6e9062c0b8cda4b788b008517996fb41e9e
Guilherme Lúcio <guilhermecaixeta@users.noreply.github.com>
2018-11-12 10:10:50 -0200

    Atualização modelo

--- src/main/java/com/ifg/sistema/sisgesport/api/entities/Equipe.java
+++ src/main/java/com/ifg/sistema/sisgesport/api/entities/Equipe.java
@@ -71,8 +71,9 @@ public class Equipe implements Serializable {
 	private List<Time> time = new ArrayList<>();
 
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "equipe_aluno", joinColumns = @JoinColumn(name = "equipe", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "aluno", referencedColumnName = "id"), uniqueConstraints = {
-			@UniqueConstraint(columnNames = { "equipe", "aluno" }) })
+	@JoinTable(name = "equipe_aluno", joinColumns = @JoinColumn(name = "equipe", referencedColumnName = "id"), 
+		   inverseJoinColumns = @JoinColumn(name = "aluno", referencedColumnName = "id"), uniqueConstraints = {
+			@UniqueConstraint(columnNames = { "aluno", "equipe" }) })
 	private List<Aluno> aluno = new ArrayList<>();
 
 	public Equipe() {
