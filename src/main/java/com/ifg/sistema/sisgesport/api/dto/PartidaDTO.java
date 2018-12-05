33771b6b62490489fda0aa77988b14d44827424d
Guilherme Lúcio <guilhermecaixeta@users.noreply.github.com>
2018-12-05 14:37:20 -0200

    Atualização entidadeDTO

--- src/main/java/com/ifg/sistema/sisgesport/api/dto/PartidaDTO.java
+++ src/main/java/com/ifg/sistema/sisgesport/api/dto/PartidaDTO.java
@@ -7,11 +7,6 @@ import java.util.List;
 import com.ifg.sistema.sisgesport.api.dto.commom_entities.EntidadeComumDTO;
 import com.ifg.sistema.sisgesport.api.dto.commom_entities.PessoaDTO;
 import com.ifg.sistema.sisgesport.api.dto.dto_retorno.TimeRetornoDTO;
-import com.ifg.sistema.sisgesport.api.entities.Evento;
-import com.ifg.sistema.sisgesport.api.entities.Modalidade;
-import com.ifg.sistema.sisgesport.api.entities.PartidaPenalidade;
-import com.ifg.sistema.sisgesport.api.entities.PartidaPonto;
-import com.ifg.sistema.sisgesport.api.entities.Time;
 import com.ifg.sistema.sisgesport.api.entities.commom_entities.Pessoa;
 
 public class PartidaDTO extends EntidadeComumDTO {
@@ -23,7 +18,7 @@ public class PartidaDTO extends EntidadeComumDTO {
 	private TimeRetornoDTO timeCasa;
 	private TimeRetornoDTO timeVisita;
 	private EventoDTO evento;
-	private ModalidadeDTO modalidade;
+	private EventoModalidadeDTO eventoModalidade;
 	private List<PartidaPenalidadeDTO> partidaPenalidade = new ArrayList<PartidaPenalidadeDTO>();
 	private List<PartidaPontoDTO> partidaPonto = new ArrayList<PartidaPontoDTO>();
 
@@ -94,12 +89,12 @@ public class PartidaDTO extends EntidadeComumDTO {
 		this.evento = evento;
 	}
 
-	public ModalidadeDTO getModalidade() {
-		return modalidade;
+	public EventoModalidadeDTO getEventoModalidade() {
+		return eventoModalidade;
 	}
 
-	public void setModalidade(ModalidadeDTO modalidade) {
-		this.modalidade = modalidade;
+	public void setEventoModalidade(EventoModalidadeDTO eventoModalidade) {
+		this.eventoModalidade = eventoModalidade;
 	}
 
 	public List<PartidaPenalidadeDTO> getPartidaPenalidade() {
